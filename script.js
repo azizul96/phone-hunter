012122e0858d95dd374a6aa8b1fe51fd48d9ce43
azizul96 <alifalamin42@gmail.com>
2023-08-28 17:15:52 +0600

    modal added

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-const allPhone = (searchText) =>{
+const allPhone = (searchText, isShowAll) =>{
     fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
     .then(response => response.json())
-    .then(data => loadData(data.data))
+    .then(data => loadData(data.data, isShowAll))
 }
 
 
-const loadData = phones =>{
+const loadData = (phones, isShowAll) =>{
     const phonContainer = document.getElementById('phone-container')
     phonContainer.textContent = "";
 
     const showAllButton = document.getElementById('show-all-btn')
-    if(phones.length > 12){
+    if(phones.length > 12 && !isShowAll){
         showAllButton.classList.remove('hidden')
     }
     else{
         showAllButton.classList.add('hidden')
     }
-    phones = phones.slice(0,12)
+    
+    if(!isShowAll){
+        phones = phones.slice(0,12)
+    }
     
 
     phones.forEach(phoneElement => {
@@ -32,15 +35,58 @@ const loadData = phones =>{
                         
                         <h2 id="card1-name" class="text-[#111] font-semibold">${phoneElement.phone_name}</h2>
                         <p class="text-[#111] opacity-70">${phoneElement.brand}</p>
+                        <button onclick="detailsHandler('${phoneElement.slug}')" class="btn btn-accent btn-sm text-white">Details</button>
                     </div>
                  `
         phonContainer.appendChild(phoneCard)
     })
+    toggleSpinner(false)
 }
 
-const searchHandle =() =>{
+const detailsHandler = (id) =>{
+    console.log('clicked' ,id);
+    const modalData = fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+    .then(response => response.json())
+    .then(detailsData => showModalDetails(detailsData.data) )
+    
+}
+const showModalDetails = (details) => {
+    console.log(details);
+    show_modal.showModal()
+    const modalContainer = document.getElementById('modal-container')
+    modalContainer.innerHTML = `
+    
+    <div class=" flex justify-center" >
+    <img src="${details.image}" alt="" class="max-h-32 rounded-xl  " />
+    </div>
+    <h3 class="font-bold text-lg text-center ">${details.name}</h3>
+    <p><span class="font-semibold">Storage: </span>${details.mainFeatures.storage}</p>
+    <button class="mt-5 btn btn-accent btn-sm text-white bg-red-600 border-none ">Close</button>
+    `
+    
+}
+
+
+
+const searchHandle =(isShowAll) =>{
+    toggleSpinner(true)
     const searchField = document.getElementById('search-field')
     const searchText = searchField.value
-    allPhone(searchText)
+    allPhone(searchText, isShowAll)
     
+}
+
+const toggleSpinner = (isLoading) => {
+    const loadingSpinner = document.getElementById('loading-spinner')
+    if(isLoading){
+        loadingSpinner.classList.remove('hidden')
+    }
+    else{
+        loadingSpinner.classList.add('hidden')
+    }
+
+}
+
+const HandleShowAll = () => {
+    searchHandle(true)
 }
